d48600db3b77df345e2fbbd1d0ca1249c422bc52
Marshall Allen Beddoe <unmarshal@gmail.com>
2015-05-06 20:43:41 -0700

    install vim bundles script

--- install_bundles.sh
+++ install_bundles.sh
@@ -2,84 +2,37 @@
 
 set -e
 
-mkdir -p vim/bundle
-cd vim/bundle
+VIM_BUNDLE_DIR=~/.vim/bundle
 
-# FuzzyFinder
+mkdir -p $VIM_BUNDLE_DIR
+cd $VIM_BUNDLE_DIR
+
+# Must Haves
 git clone git@github.com:vim-scripts/FuzzyFinder.git
-
-# LustyJuggler
 git clone git@github.com:vim-scripts/LustyJuggler.git
-
-# VimClojure
-git clone git@github.com:vim-scripts/VimClojure.git
-
-# clang_complete
-git clone git@github.com:Rip-Rip/clang_complete.git
-
-# hive.vim
-git clone git@github.com:autowitch/hive.vim.git
-
-# nerdtree
 git clone git@github.com:scrooloose/nerdtree.git
-
-# pig.vim
-git clone git@github.com:motus/pig.vim.git
-
-# rust.vim
-git clone git@github.com:rust-lang/rust.vim.git
-
-# vim-snippets (formally snipmate-snippets)
-git clone git@github.com:honza/vim-snippets.git
-
-# tComment
-# git clone https://github.com/vim-scripts/tComment
 git clone git@github.com:tomtom/tcomment_vim.git
-
-# tlib
 git clone git@github.com:tomtom/tlib_vim.git
-
-# vim-addon-mw-utils
-git clone git@github.com:vim-scripts/vim-addon-mw-utils.git
-
-# vim-bufexplorer
-git clone git@github.com:jlanzarotta/bufexplorer.git
-
-# vim-coffee-script
-git clone git@github.com:kchmck/vim-coffee-script.git
-
-# vim-easymotion
-git clone git@github.com:Lokaltog/vim-easymotion.git
-
-# vim-fugitive
-git clone git@github.com:tpope/vim-fugitive.git
-
-# vim-go
-git clone git@github.com:fatih/vim-go.git
-
-# vim-javascript
 git clone git@github.com:pangloss/vim-javascript.git
-
-# vim-l9
-git clone git@github.com:clones/vim-l9.git
-
-# vim-node
-git clone git@github.com:moll/vim-node.git
-
-# vim-scala
-git clone git@github.com:derekwyatt/vim-scala.git
-
-# vim-sensible
 git clone git@github.com:tpope/vim-sensible.git
 
-# vim-slime
-git clone git@github.com:jpalardy/vim-slime.git
-
-# vim-snipmate
-git clone git@github.com:msanders/snipmate.vim.git
-
-# vim-sparkup
+git clone git@github.com:jlanzarotta/bufexplorer.git
+git clone git@github.com:Lokaltog/vim-easymotion.git
+git clone git@github.com:tpope/vim-fugitive.git
+git clone git@github.com:clones/vim-l9.git
 git clone git@github.com:tristen/vim-sparkup.git
-
-# vim-supertab
 git clone git@github.com:tsaleh/vim-supertab.git
+
+# git clone git@github.com:vim-scripts/vim-addon-mw-utils.git
+
+# Language Specific
+
+git clone git@github.com:moll/vim-node.git
+git clone git@github.com:Rip-Rip/clang_complete.git
+git clone git@github.com:rust-lang/rust.vim.git
+git clone git@github.com:fatih/vim-go.git
+# git clone git@github.com:derekwyatt/vim-scala.git
+# git clone git@github.com:motus/pig.vim.git
+# git clone git@github.com:autowitch/hive.vim.git
+# git clone git@github.com:kchmck/vim-coffee-script.git
+# git clone git@github.com:vim-scripts/VimClojure.git
